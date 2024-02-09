06a86a8517d6470b8733d56cc8edfb5e0a283f0e
tanvir243 <travelertanvir@gmail.com>
2024-02-09 12:04:34 +0600

    task-2 completerd

--- solution/task-2.js
+++ solution/task-2.js
@@ -1,1 +1,14 @@
 // task-2
+const heights2 = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
+
+function shortestName(array){
+    let shortestLengthName = array[4];
+    for(const iterator of array){
+        if(iterator.length < shortestLengthName.length){
+            shortestLengthName = iterator;
+        }
+    }
+    return shortestLengthName;
+}
+const finalName = shortestName(heights2);
+console.log(finalName);
